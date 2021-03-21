90a89bd98a5166b131e42fb7588ad4554084f3d8
pirenato <pinter.renato@gmail.com>
2021-03-21 22:23:03 +0100

    Creating class for creating table and inserting records into an h2 database

---
+++ src/main/java/org/example/H2CreateTable.java
@@ -0,0 +1,64 @@
+package org.example;
+
+import java.sql.Connection;
+import java.sql.DriverManager;
+import java.sql.SQLException;
+import java.sql.Statement;
+
+public class H2CreateTable {
+    static final String JDBC_DRIVER = "org.h2.Driver";
+    static final String DB_URL = "jdbc:h2:~/test";
+
+    //Database credentials
+    static final String USER = "sa";
+    static final String PASS = "";
+
+    public static void main(String[] args) {
+        Connection conn = null;
+        Statement stmt = null;
+        try {
+            //Register JDBC driver
+            Class.forName(JDBC_DRIVER);
+
+            //Open a connection
+            System.out.println("Connecting to database...");
+            conn = DriverManager.getConnection(DB_URL, USER, PASS);
+
+            //Execute a query
+            System.out.println("Creating table in given database...");
+            stmt = conn.createStatement();
+            String sql = "CREATE TABLE IF NOT EXISTS    USERS " +
+                    "(id INTEGER not NULL, " +
+                    " first_name VARCHAR(255), " +
+                    " last_name VARCHAR(255), " +
+                    " age INTEGER, " +
+                    " PRIMARY KEY ( id ))";
+            stmt.executeUpdate(sql);
+            System.out.println("Created table in given database...");
+
+            //Clean-up environment
+            stmt.close();
+            conn.close();
+        } catch (SQLException se) {
+            //Handle errors for JDBC
+            se.printStackTrace();
+        } catch (Exception e) {
+            //Handle errors for Class.forName
+            e.printStackTrace();
+        } finally {
+            //block used to close resources
+            try {
+                if (stmt != null) stmt.close();
+            }
+            catch (SQLException throwables) {
+                throwables.printStackTrace();
+            }
+            try {
+                if (conn != null) conn.close();
+            } catch (SQLException se) {
+                se.printStackTrace();
+            } //end finally try
+        } //end try
+        System.out.println("Shutting down!");
+    }
+}
